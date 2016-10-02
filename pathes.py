0824c62feceb66e1eebaef851a1635d27441b457
saneku <sasha.ukhov@gmail.com>
2016-10-02 18:33:19 +0300

    added full gocart

--- pathes.py
+++ pathes.py
@@ -1,8 +1,8 @@
-wrf_dir="/home/ukhova/Downloads/"
+wrf_dir="/home/ukhova/Apps/WRF/V3.7.1/WRFV3.7.1/run_visuvi_tutorial/"
 wrf_input_file="wrfinput_d01"
 wrf_bdy_file="wrfbdy_d01"
 
-wrf_met_dir="/home/ukhova/Downloads/"
+wrf_met_dir="/home/ukhova/Apps/WRF/V3.7.1/WRFV3.7.1/run_visuvi_tutorial/"
 wrf_met_files="met_em.d01.2010*"
 
 mera_dir="/home/ukhova/Downloads/Merra2ForVISUVI_data/"
@@ -21,7 +21,26 @@ spc_map = [ 'DUST_1 -> 1*[DU001];1.e9',
             'DUST_4 -> 1*[DU004];1.e9',
             'DUST_5 -> 1.0*[DU005];1.e9']
 
+#GOCART FULL
+spc_map = [ 'DUST_1 -> 1*[DU001];1.e9',
+            'DUST_2 -> 1*[DU002];1.e9',
+            'DUST_3 -> 1.*[DU003];1.e9',
+            'DUST_4 -> 1*[DU004];1.e9',
+            'DUST_5 -> 1.0*[DU005];1.e9',
+            'SEAS_1 -> 1.0*[SS002];1.e9',
+            'SEAS_2 -> 1.0*[SS003];1.e9',
+            'SEAS_3 -> 1.0*[SS004];1.e9',
+            'SEAS_4 -> 1.0*[SS005];1.e9',
+            'so2 -> 0.453*[SO2];1.e6',
+            'sulf -> 0.302*[SO4];1.e6',
+            'BC1 -> 0.4143*[BCPHOBIC];1.e9', 'BC2 -> 0.4143*[BCPHILIC];1.e9',
+            'OC1 -> 0.4143*[OCPHOBIC];1.e9', 'OC2 -> 0.4143*[OCPHILIC];1.e9',
+            'o3 -> 0.604*[O3];1.e6',
+            'co -> 0.965*[CO];1.e6']
 
+#DMS and MSA ?
+
+'''
 #CBMZ-MOSAIC_8bins  DUST only
 spc_map =['oin_a02->4.92e-4*[DU001];1.e9',
           'oin_a03->8.94e-3*[DU001];1.e9',
@@ -38,3 +57,6 @@ spc_map =['oin_a02->4.92e-4*[DU001];1.e9',
           'num_a06->2.086e13*[DU002]+0.358e13*[DU003];1',
           'num_a07->0.255e13*[DU003]+0.592e12*[DU004];1',
           'num_a08->0.296e12*[DU004]+1.655e11*[DU005];1']
+'''
+
+#python ./main.py > proc.log 2>&1
