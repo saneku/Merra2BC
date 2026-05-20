09bbeb7d177a7c197fed5f2a110807d5c2cdb91d
Sateesh <sateesh.masabathini@kaust.edu.sa>
2026-05-20 17:08:01 +0300

    variables sorted.

--- zero_fields.py
+++ zero_fields.py
@@ -9,9 +9,9 @@ start_time = time.time()
 
 zero = 1e-16
 #For GOCART
-fields_to_zero=['DUST_1','DUST_2','DUST_3','DUST_4','DUST_5',
-                'so2','sulf','SEAS_1','SEAS_2','SEAS_3',
-                'SEAS_4', 'BC1','BC2','OC1','OC2','dms', 'o3', 'msa', 'nh3', 'co', 'co2']
+fields_to_zero=['BC1', 'BC2', 'DUST_1', 'DUST_2', 'DUST_3', 'DUST_4', 'DUST_5',
+                'OC1', 'OC2', 'SEAS_1', 'SEAS_2', 'SEAS_3', 'SEAS_4',
+                'co', 'co2', 'dms', 'msa', 'nh3', 'o3', 'so2', 'sulf']
 
 '''
 #For MOSAIC 8 bins
